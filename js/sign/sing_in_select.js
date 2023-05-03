59b37e74b2403c460a82c271ba7b8b09a4c1465f
KSY0606 <tkddbs9506@naver.com>
2023-05-03 16:54:24 +0900

    고통파일수정

--- js/sign/sing_in_select.js
+++ js/sign/sing_in_select.js
@@ -1,4 +1,4 @@
-
+/*
 $(document).ready(function(){
     $("#sign_in_select > p:nth-child(1)").click(function(){
         $('#sign_in_info').css('display', 'block');
@@ -20,4 +20,39 @@ $(document).ready(function(){
         $('#sign_in_select > p:nth-child(1)').css('color', 'black');
         $('#sign_in_select > p:nth-child(2)').css('color', 'white');
     })
-})
+})*/
+
+
+$(document).ready(function() {
+    var signInSelect = $("#sign_in_select > p");
+    var signInInfo = $('#sign_in_info');
+    var nonInfo = $('#non_info');
+    
+    signInSelect.click(function() {
+        var index = signInSelect.index(this);
+        
+        if (index === 0) {
+            signInInfo.show();
+            nonInfo.hide();
+            signInSelect.eq(0).css({
+            backgroundColor: '#555',
+            color: 'white'
+            });
+            signInSelect.eq(1).css({
+            backgroundColor: 'white',
+            color: 'black'
+            });
+        } else {
+            signInInfo.hide();
+            nonInfo.show();
+            signInSelect.eq(1).css({
+            backgroundColor: '#555',
+            color: 'white'
+            });
+            signInSelect.eq(0).css({
+            backgroundColor: 'white',
+            color: 'black'
+            });
+        }
+    });
+});
